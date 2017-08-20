5b40fe5ee4b5a7f559f965036988361dfad208bb
Sam Kauffman <skauffman@kmbs.konicaminolta.us>
2017-08-20 11:28:09 -0400

    add full resume

--- src/script/pages/Resume/index.jsx
+++ src/script/pages/Resume/index.jsx
@@ -28,13 +28,35 @@ export default class Home extends React.Component {
                                 </ul>
                             </section>
                         </article>
+
                         <article>
-                            <h2>Education</h2>
+                            <h2>Experience</h2>
                             <section>
-                                <h3>University of Massachusetts Amherst, 2011 - 2015</h3>
+                                <h3>Konica Minolta, August 2015 - Now</h3>
                                 <ul>
-                                    <li>B.S. in Computer Science</li>
-                                    <li>Minor in Anthropology</li>
+                                    <li>Web development using Python, vanilla JS</li>
+                                    <li>Maintain legacy code</li>
+                                    <li>Create web applications</li>
+                                </ul>
+                            </section>
+                            <section>
+                                <h3>UMass Athletics Tutor, June - August 2014</h3>
+                                <ul>
+                                    <li>Tutor students in CS and calculus</li>
+                                    <li>Help them work through concepts and ideas</li>
+                                </ul>
+                            </section>
+                        </article>
+
+                        <article>
+                            <h2>Skills</h2>
+                            <section>
+                                <h3>Konica Minolta, August 2015 - Now</h3>
+                                <ul>
+                                    <li>Proficient with Python, Javascript, HTML, CSS</li>
+                                    <li>Some experience with Go, Java, C++, SQL</li>
+                                    <li>Git for version control</li>
+                                    <li>Linux for development</li>
                                 </ul>
                             </section>
                         </article>
