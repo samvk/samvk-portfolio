f4ba3f0074e377cd40a7842e8c7642c25c7ea6f6
Sam Kauffman <skauffman@kmbs.konicaminolta.us>
2017-08-20 03:18:27 -0400

    WIP::copy About content onto Home

--- src/script/pages/Home/index.jsx
+++ src/script/pages/Home/index.jsx
@@ -18,9 +18,30 @@ export default class Home extends React.Component {
                     title='Home'
                 >
                     <div>
-                        <p>I'm a software developer living in Northampton Massachusetts.</p>
-                        <p>I currently work in web development, and am most comfortable with Python and Javascript.</p>
-                        <p>I'm interested in websites, networks, and video games.</p>
+                        <p>
+                            Hi, my name's Sam. I'm a twenty-something year old front-end web developer who loves building apps and bringing others' designs to life. My go-to technologies right now are HTML5, CSS3, Sass(SCSS), and JavaScript(ES6) + jQuery.  I'm in the process of learning PHP &amp; MySQL to become a better front-end coder and future full-stack developer.
+                        </p>
+                        <p>
+                            On a personal note: I love metal detecting, watching movies, the theatre, and building things.
+                        </p>
+                        <p>
+                            You can <Link
+                                external
+                                text
+                                to='https://github.com/samvk'
+                            >
+                                check me out on GitHub
+                            </Link>, <Link
+                                external
+                                text
+                                to='https://linkedin.com/in/samvk'
+                            >
+                                find me on LinkedIn
+                            </Link>, <Link
+                                text
+                                to='docs/SamKauffman_Resume.pdf'
+                            >take a look at my resume</Link>, or view some of my projects below.
+                        </p>
                     </div>
                 </PortfolioCard>
             </article>
