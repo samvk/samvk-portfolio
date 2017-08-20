0fb29eac93cf8786fe25d67b7c0b7a9979c3361e
Sam Kauffman <skauffman@kmbs.konicaminolta.us>
2017-08-20 03:37:09 -0400

    add headshot to home

--- src/script/pages/Home/index.jsx
+++ src/script/pages/Home/index.jsx
@@ -5,19 +5,26 @@ import pageTitle from 'react-document-title-decorator';
 import CSSModules from 'react-css-modules';
 import styles from './style.css';
 
+import Image from 'components/Image';
 import Link from 'components/Link';
 import PortfolioCard from 'components/PortfolioCard';
 
 @pageTitle('Portfolio')
-// @CSSModules(styles, { allowMultiple: true, errorWhenNotFound: false })
+@CSSModules(styles)
 export default class Home extends React.Component {
     render() {
         return (
             <article>
                 <PortfolioCard
-                    title='Home'
+                    title={
+                        <Image
+                            src='headshot.png'
+                            styleName='headshot'
+                        />
+                    }
                 >
                     <div>
+                        <h1 className='text-center'>Hi, I'm Emma.</h1>
                         <p>
                             Hi, my name's Sam. I'm a twenty-something year old front-end web developer who loves building apps and bringing others' designs to life. My go-to technologies right now are HTML5, CSS3, Sass(SCSS), and JavaScript(ES6) + jQuery.  I'm in the process of learning PHP &amp; MySQL to become a better front-end coder and future full-stack developer.
                         </p>
