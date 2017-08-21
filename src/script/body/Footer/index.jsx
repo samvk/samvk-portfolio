f71636409eca85e576c1cd61d63b8eb4c4e0b02c
Sam Kauffman <skauffman@kmbs.konicaminolta.us>
2017-08-20 23:20:37 -0400

    add styling to footer

--- src/script/body/Footer/index.jsx
+++ src/script/body/Footer/index.jsx
@@ -5,7 +5,9 @@ import classNames from 'classnames';
 import CSSModules from 'react-css-modules';
 import styles from './style.css';
 
+import Flex from 'components/Flex';
 import Image from 'components/Image';
+import Link from 'components/Link';
 
 @CSSModules(styles)
 export default class Footer extends React.Component {
@@ -27,15 +29,39 @@ export default class Footer extends React.Component {
                 {...props}
             >
                 <div className='relative'>
-                    <div>
-                        <h6 styleName='message' title='React'>
-                        Powered by&nbsp;<Image styleName='icon' src='react.png' />
-                        </h6>
-                        <h6 styleName='message' title='Raspberry Pi'>
-                        Hosted on my&nbsp;<Image styleName='icon' src='raspberry.png' /><Image styleName='icon' src='pie.png' />
-                        </h6>
-                    </div>
-                    <span styleName='copyright'>2017 &copy; Emma Simon</span>
+                    <Flex xCenter>
+                        <Flex xBetween yCenter styleName='content'>
+                            <Link
+                                external
+                                to='mailto:emmamaysimon@gmail.com'
+                                title='EmmaMaySimon@gmail.com'
+                            >
+                                <span
+                                    className='fa fa-fw fa-envelope'
+                                    styleName='logo'
+                                />
+                            </Link>
+                            <div>
+                                <h6 styleName='message' title='React'>
+                            Powered by&nbsp;<Image styleName='icon' src='react.png' />
+                                </h6>
+                                <h6 styleName='message' title='Raspberry Pi'>
+                            Hosted on my&nbsp;<Image styleName='icon' src='raspberry.png' /><Image styleName='icon' src='pie.png' />
+                                </h6>
+                            </div>
+                            <Link
+                                external
+                                to='https://linkedin.com/in/emma-simon/'
+                                title='LinkedIn'
+                            >
+                                <span
+                                    className='fa fa-fw fa-linkedin'
+                                    styleName='logo'
+                                />
+                            </Link>
+                        </Flex>
+                    </Flex>
+                    <div styleName='copyright'>2017 &copy; Emma Simon</div>
                 </div>
             </footer>
         );
