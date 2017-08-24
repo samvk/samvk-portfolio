02ec7c2bd7dc944a3163a4ebf922ecaecb891f52
Sam Kauffman <skauffman@kmbs.konicaminolta.us>
2017-08-23 20:32:37 -0400

    move form props into state

--- src/script/pages/Contact/index.jsx
+++ src/script/pages/Contact/index.jsx
@@ -12,6 +12,14 @@ import PortfolioCard from 'components/PortfolioCard';
 @pageTitle('Contact')
 // @CSSModules(styles)
 export default class Home extends React.Component {
+
+    state = {
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+    }
+
     render() {
         return (
             <article>
@@ -19,10 +27,54 @@ export default class Home extends React.Component {
                     title='Contact'
                 >
                     <form>
-                        <Input title='Name' required />
-                        <Input title='Email Address' type='email' required />
-                        <Input title='Phone Number' />
-                        <Textarea title='Message' required />
+                        <Input
+                            title='Name'
+                            required
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                this.setState(() => ({
+                                    name: value,
+                                }));
+                            }}
+                        >
+                            {this.state.name}
+                        </Input>
+                        <Input
+                            title='Email Address'
+                            type='email'
+                            required
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                this.setState(() => ({
+                                    email: value,
+                                }));
+                            }}
+                        >
+                            {this.state.email}
+                        </Input>
+                        <Input
+                            title='Phone Number'
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                this.setState(() => ({
+                                    phone: value,
+                                }));
+                            }}
+                        >
+                            {this.state.phone}
+                        </Input>
+                        <Textarea
+                            title='Message'
+                            required
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                this.setState(() => ({
+                                    message: value,
+                                }));
+                            }}
+                        >
+                            {this.state.message}
+                        </Textarea>
                         <button className='full-width' type='submit'>Submit</button>
                     </form>
                 </PortfolioCard>
