b46d2ad3eaa3c39f54e52a180ea1ee921876943c
Sam Kauffman <samvnkauffman@gmail.com>
2017-12-10 18:56:52 -0500

    WIP::project cards

---
+++ src/script/components/ProjectCard/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import CSSModules from 'react-css-modules';
+import styles from './style.css';
+
+import Image from 'components/Image';
+import Link from 'components/Link';
+import Flex from 'components/Flex';
+import Logo from 'components/Logo';
+
+@CSSModules(styles, { allowMultiple: true })
+export default class ProjectCard extends React.Component {
+    static propTypes = {
+        audio: PropTypes.bool, // mark if link will have sound
+        featured: PropTypes.bool,
+        href: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        src: PropTypes.string.isRequired,
+    }
+
+    static defaultProps = {
+        featured: false,
+    }
+
+
+    render() {
+        const { audio, featured, name, href, src, ...props } = this.props;
+        return (
+            <div
+                styleName='container'
+                {...props}
+            >
+                <Link
+                    className='relative'
+                    href={href}
+                >
+                    <Image
+                        styleName={classNames('image', { featured })}
+                        src={src}
+                        altTitle={name}
+                    />
+                    {audio && <Logo styleName='audio' logo='volume-up' />}
+                </Link>
+                <Flex xCenter>
+                    <Logo
+                        logo='github-square'
+                        href='https://github.com/samvk'
+                        title='GitHub'
+                        text
+                    />
+                </Flex>
+            </div>
+        );
+    }
+}
--- src/script/pages/Projects/index.jsx
+++ src/script/pages/Projects/index.jsx
@@ -5,11 +5,12 @@ import pageTitle from 'react-document-title-decorator';
 import CSSModules from 'react-css-modules';
 import styles from './style.css';
 
-import Link from 'components/Link';
+import Flex from 'components/Flex';
 import PortfolioCard from 'components/PortfolioCard';
+import ProjectCard from 'components/ProjectCard';
 
 @pageTitle('Projects')
-// @CSSModules(styles, { allowMultiple: true })
+@CSSModules(styles, { allowMultiple: true })
 export default class Home extends React.Component {
     render() {
         return (
@@ -17,9 +18,59 @@ export default class Home extends React.Component {
                 <PortfolioCard
                     title='Projects'
                 >
-                    <div>
-                        <p>Projects</p>
-                    </div>
+                    <h1>For the Web</h1>
+                    <Flex wrap styleName='grid'>
+                        <ProjectCard
+                            featured
+                            name='Konica Minolta Marketplace'
+                            href='https://us.konicaminoltamarketplace.com'
+                            src='projects/konicaminoltamarketplace.png'
+                        />
+                        <ProjectCard
+                            name='Greetings, World!'
+                            href='https://greetingsworld.us'
+                            src='projects/greetingsworld.png'
+                        />
+                        <ProjectCard
+                            name='The Treasure Hunter'
+                            href='https://cttreasurehunter.com'
+                            src='projects/cttreasurehunter.png'
+                        />
+                        <ProjectCard
+                            name='Read Every Day!'
+                            href='https://readeverydayclub.com'
+                            src='projects/readeveryday.png'
+                        />
+                        <ProjectCard
+                            name='Date Russell'
+                            href='https://daterussell.com'
+                            src='projects/daterussell.png'
+                        />
+                        <ProjectCard
+                            name='SamVK | My Portfolio'
+                            href='https://samvk.com'
+                            src='projects/samvk.png'
+                        />
+                    </Flex>
+                    <h1>Just for Fun</h1>
+                    <Flex wrap styleName='grid'>
+                        <ProjectCard
+                            audio
+                            name='Team Fortress 2'
+                            href='https://teamfortress2.samvk.com'
+                            src='projects/teamfortress2.png'
+                        />
+                        <ProjectCard
+                            name='Snake'
+                            href='https://snake.samvk.com'
+                            src='projects/snake.png'
+                        />
+                        <ProjectCard
+                            name='Metal Detecting'
+                            href='https://cttreasurehunter.com/metaldetecting'
+                            src='projects/metaldetecting.png'
+                        />
+                    </Flex>
                 </PortfolioCard>
             </article>
         );
