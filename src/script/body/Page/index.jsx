10024dca2a2cc32d4d3d561ddbdb0dc2778fb492
Sam Kauffman <samvnkauffman@gmail.com>
2017-12-10 16:49:10 -0500

    scroll to top on page switch

--- src/script/body/Page/index.jsx
+++ src/script/body/Page/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 import About from 'pages/About';
 import Projects from 'pages/Projects';
 import Contact from 'pages/Contact';
 import FourOhFour from 'pages/FourOhFour';
 
+@withRouter
 export default class Page extends React.Component {
+    componentDidUpdate(prevProps) {
+        if (this.props.location !== prevProps.location) {
+            window.scrollTo(0, 0); // scroll to top on page switch
+        }
+    }
+
     render() {
         return (
             <Switch>
